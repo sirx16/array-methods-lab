7e00fb9d94f9ed41663a77c5c0663b8f6fe9f59e
sirx16 <atonuslperry@gmail.com>
2020-10-27 11:02:56 -0600

    commit tree fc4f1438f00bfd592f97106f2c9a38658ec69c96

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
 def using_include(array, element)
-
-end
-
-def using_sort(array)
-
-end
-
-def using_reverse(array)
-
-end
-
-def using_first(array)
-
-end
-
-def using_last(array)
-
-end
-
-def using_size(array)
-
+    array.include? (element)
+ end
+ def using_sort(array)
+    ["Wow" , "I", "am" , "learning", "arrays"]
+    array.sort
+ end
+ def using_reverse(array)
+    ["Wow" , "I", "am" , "learning", "arrays"]
+    array.reverse
+ end
+ def using_first(array)
+    ["Wow" , "I", "am" , "learning", "arrays"]
+    array.first
+ end
+ def using_last(array)
+    ["Wow" , "I", "am" , "learning", "arrays"]
+    array.last
+ end
+ def using_size(array)
+    ["Wow" , "I", "am" , "learning", "arrays"]
+    array.size 
 end
